6ff4868842131c264ac5821375ca7d1c0dfd31cc
Luis Martins <127532072+lm27nba@users.noreply.github.com>
2023-05-16 22:56:30 +0100

    ultimas alterações JS

--- Public/JavaScript/functions.js
+++ Public/JavaScript/functions.js
@@ -22,8 +22,7 @@ function voltar_atras(){ window.history.back(); }
 // Função para fazer o scroll:
 function scrollInicial(){
 
-    let scroll_inicial = document.getElementById("caixa_entrada");
-    
+    let scroll_inicial = document.getElementById("caixa_entrada");   
     scroll_inicial.scrollIntoView();
 }
 
@@ -37,4 +36,59 @@ function mostrar_conteudo(){
     div.style.display = "none";
     div_nova.style.display = "block";
     botao.style.display = "none";
+}
+
+// Função para trocar os botões na NAV-BAR:
+function trocarBotaoNav(){
+
+    let menu = document.getElementById("btn_nav_menu");
+    let fechar = document.getElementById("btn_nav_fechar");
+
+    if(menu.style.display === "block"){
+        menu.style.display = "none";
+        fechar.style.display = "block";
+    }
+
+    else{
+        fechar.style.display = "none";
+        menu.style.display = "block";
+    }
+}
+
+// Função para efeito hover das setas de paginação:
+
+// Hover na seta da esquerda:
+function trocarseta(){
+    let seta = document.getElementById("setaesq");
+    let seta1 = document.getElementById("setaesq1");
+
+    seta.style.display = "none";
+    seta1.style.display = "block";
+}
+
+// Regressar ao normal na seta da esquerda:
+function voltarseta(){
+    let seta = document.getElementById("setaesq");
+    let seta1 = document.getElementById("setaesq1");
+
+    seta.style.display = "block";
+    seta1.style.display = "none";
+}
+
+// Hover na seta da direita:
+function trocarseta1(){
+    let seta = document.getElementById("setadir");
+    let seta1 = document.getElementById("setadir1");
+
+    seta.style.display = "none";
+    seta1.style.display = "block";
+}
+
+// Regressar ao normal na seta da direita:
+function voltarseta1(){
+    let seta = document.getElementById("setadir");
+    let seta1 = document.getElementById("setadir1");
+
+    seta.style.display = "block";
+    seta1.style.display = "none";
 }
--- Public/JavaScript/main.js
+++ Public/JavaScript/main.js
@@ -2,57 +2,11 @@
 // Fazer compasso de espera antes de executar o scroll automático nas diversas páginas:
 setTimeout(scrollInicial, 400);
 
-// Função para trocar os botões na NAV-BAR:
-function trocarBotaoNav(){
 
-    let menu = document.getElementById("btn_nav_menu");
-    let fechar = document.getElementById("btn_nav_fechar");
+// Garantir que a função scroll inicial é sempre recarregada. Não estava a funcionar quando vinha de window.history.back()!
+window.onpageshow = function(event) {
+    if (event.persisted) {
+        setTimeout(scrollInicial, 400);
 
-    if(menu.style.display === "block"){
-        menu.style.display = "none";
-        fechar.style.display = "block";
     }
-
-    else{
-        fechar.style.display = "none";
-        menu.style.display = "block";
-    }
-}
-
-// Função para efeito hover das setas de paginação:
-
-// Hover na seta da esquerda:
-function trocarseta(){
-    let seta = document.getElementById("setaesq");
-    let seta1 = document.getElementById("setaesq1");
-
-    seta.style.display = "none";
-    seta1.style.display = "block";
-}
-
-// Regressar ao normal na seta da esquerda:
-function voltarseta(){
-    let seta = document.getElementById("setaesq");
-    let seta1 = document.getElementById("setaesq1");
-
-    seta.style.display = "block";
-    seta1.style.display = "none";
-}
-
-// Hover na seta da direita:
-function trocarseta1(){
-    let seta = document.getElementById("setadir");
-    let seta1 = document.getElementById("setadir1");
-
-    seta.style.display = "none";
-    seta1.style.display = "block";
-}
-
-// Regressar ao normal na seta da direita:
-function voltarseta1(){
-    let seta = document.getElementById("setadir");
-    let seta1 = document.getElementById("setadir1");
-
-    seta.style.display = "block";
-    seta1.style.display = "none";
-}
+};
